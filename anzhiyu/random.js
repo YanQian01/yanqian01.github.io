03ac91c70993080c569cb4ad72944a352b7fcd5f
yanqian <2742094445@qq.com>
2023-06-16 19:39:35 +0800

    Site updated: 2023-06-16 19:39:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/06/16/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/06/16/hello-world/","2023/06/16/健康作息/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
